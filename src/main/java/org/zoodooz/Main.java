c81ffcdf06d9e4084f087aab552ba749ded88c0a
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 16:53:03 +0200

    Przebyta odległość

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,9 +1,34 @@
 package org.zoodooz;
 
 
+import java.util.Scanner;
+
 public class Main {
     
     public static void main(String[] args) {
 
+        Scanner keyboard = new Scanner(System.in);
+        int hoursTrip;
+        int speed;
+        double distance = 0;
+
+        do {
+            System.out.println("Podaj prędkość: (km/h)");
+            speed = keyboard.nextInt();
+        }while (speed <=0);
+
+        do {
+            System.out.println("Podaj liczbę godzin podróży: ");
+            hoursTrip = keyboard.nextInt();
+        }while (hoursTrip <= 0);
+
+        System.out.println("Godzina \tPrzebyta odległość");
+        System.out.println("---------------------------");
+
+        for (int i = 0; i < hoursTrip; i++){
+            distance += (speed * hoursTrip) / hoursTrip;
+
+            System.out.println((i+1) + "\t\t\t" + distance + " km");
+        }
     }
 }
