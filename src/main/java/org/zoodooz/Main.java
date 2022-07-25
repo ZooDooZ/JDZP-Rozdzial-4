942cdd23b8aab2f72b1c723e41b3777127fb70e6
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 17:14:46 +0200

    Przekształcanie małych liter w pliku na wielkie

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,9 +1,31 @@
 package org.zoodooz;
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Main {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Podaj nazwę pliku do odczytu: ");
+        String fileName = keyboard.nextLine();
+
+        File file = new File(fileName); //plik do odczytu czyli wejsciowy
+        Scanner inputFile = new Scanner(file);
+
+        PrintWriter printWriter = new PrintWriter("duze litery.txt");
+
+        while (inputFile.hasNext()){
+            String line = inputFile.nextLine();
+            String bigLine = line.toUpperCase();
+            System.out.println(bigLine);
+            printWriter.println(bigLine);
+        }
+        printWriter.close();
     }
 }
