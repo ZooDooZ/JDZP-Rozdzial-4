12381c73af311b3d3a339ec1136d6fce562dfadd
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 17:13:21 +0200

    Numery wierszy

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,9 +1,28 @@
 package org.zoodooz;
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Podaj nazwę pliku: ");
+        String fileName = keyboard.nextLine();
+
+        File file = new File(fileName);
+        Scanner inoutFile = new Scanner(file);
+
+        int counter = 1;
+
+        while (inoutFile.hasNext()){
+            String line = inoutFile.nextLine();
+            System.out.println(counter + " : " + line);
+            counter++;
+        }
     }
 }
