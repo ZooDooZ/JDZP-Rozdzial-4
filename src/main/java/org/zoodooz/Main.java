f3a31902b22a747299a2b2f0c2333344635e8395
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 17:17:07 +0200

    Zabawa w odgadywanie liczby losowej

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,9 +1,31 @@
 package org.zoodooz;
 
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     
     public static void main(String[] args) {
 
+        Random randomNumber = new Random();
+        Scanner keyboard = new Scanner(System.in);
+
+        int computerNumber;
+        int playerNumber;
+
+        do {
+            computerNumber = randomNumber.nextInt(10)+1;
+
+            System.out.println("Zgadnij jaką liczbę wylosował komputer z liczb 1-10: ");
+            playerNumber = keyboard.nextInt();
+
+            if (playerNumber > computerNumber) {
+                System.out.println("Za duża liczba, spróbuj ponownie.");
+            } else if (playerNumber < computerNumber) {
+                System.out.println("Za mała liczba, spróbuj ponownie.");
+            }
+        }while (playerNumber != computerNumber);
+        System.out.println("Brawo zgadłeś! Twoja liczba: " + playerNumber + " Computera Liczba: " + computerNumber);
     }
 }
