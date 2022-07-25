9caa7003a6ab101cd070d755ad6c94bfee75a0ef
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 16:51:54 +0200

    Suma liczb

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -1,9 +1,23 @@
 package org.zoodooz;
 
 
-public class Main {
-    
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Podaj dodatnia liczbę całkowitą niezerową: ");
+        int number = keyboard.nextInt();
+
+        int x;
+        int sum = 0;
+
+        for (x = 0; x <= number; x++){
+            sum += x;
+        }
+        System.out.println(sum);
     }
 }
