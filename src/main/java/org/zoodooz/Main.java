4e67b949174f5f19d41cbce3b82ed2fcd884bcc9
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-07-25 17:09:40 +0200

    Tabela przeliczająca stopnie Celsjusza na stopnie Fahrenheita

--- src/main/java/org/zoodooz/Main.java
+++ src/main/java/org/zoodooz/Main.java
@@ -5,5 +5,13 @@ public class Main {
     
     public static void main(String[] args) {
 
+        System.out.println("Stopnie Celcjusza\t|\t Stopnie Fahrenheita");
+        System.out.println("---------------------------------------------");
+
+        for (int i = 0 ; i <= 20; i++){
+            double fahrenheita = ((double)9/5) * i + 32;
+            System.out.printf(i + " stopie C\t\t\t|\t %.1f stopni F \n", fahrenheita);
+            System.out.println("---------------------------------------------");
+        }
     }
 }
